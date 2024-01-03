d48dc236fe0dccac9d5a801a007ea966026932b9
Rajkokate <98548800+Rajkokate@users.noreply.github.com>
2024-01-03 14:38:00 +0530

    Add files via upload

---
+++ Day-1/pro2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class pro2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int area = a * a;
+        System.out.println("Area of aquare ="+ area);
+    }
+    
+}
