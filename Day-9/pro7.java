7edc395786dea5154d66e7fc710402b5f1e0ebe7
Rajkokate <98548800+Rajkokate@users.noreply.github.com>
2024-02-05 16:04:59 +0530

    Update pro7.java

--- Day-9/pro7.java
+++ Day-9/pro7.java
@@ -1,12 +1,16 @@
+//total pairs finding
 public class pro7 {
     public static void pairs(int number[]){
+        int tp =0;
         for(int i=0;i<number.length;i++){
             int curr=number[i];
             for(int j=i+1;j<number.length;j++){
                 System.out.print("(" + curr + "," + number[j] + ")");
+                tp++;
             }
             System.out.println();
         }
+        System.out.println("total pairs = "+ tp);
     }
     public static void main(String[] args) {
         int number[] = {2,4,6,8,10};
