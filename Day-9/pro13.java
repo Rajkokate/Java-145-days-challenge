3af853048ea5a653270a109c1092ef96e49f3040
Rajkokate <98548800+Rajkokate@users.noreply.github.com>
2024-02-07 20:38:19 +0530

    Add files via upload

---
+++ Day-9/pro13.java
@@ -0,0 +1,20 @@
+public class pro13 {
+    public static int BuysellStocks(int price[]){
+        int buyprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        
+        for(int i = 0;i<price.length;i++){
+            if(buyprice<price[i]){
+                int profit = price[i] - buyprice;
+                maxprofit = Math.max(maxprofit, profit);
+            }else{
+                buyprice=price[i];
+            }
+        }
+        return maxprofit;
+    }
+    public static void main(String[] args) {
+        int price[] = {7,1,5,3,6,4};
+        System.out.println(BuysellStocks(price));
+    }
+}
